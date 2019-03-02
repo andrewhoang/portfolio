0c201f1b2fb76fdfa833e62d21fefb4cbb6e7fc1
andrewhoang <a.hoang95@gmail.com>
2019-03-02 13:45:50 -0500

    switch to hash router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './assets/styles/styles.scss';
 import '../node_modules/animate.css/animate.min.css';
 import App from './components/App';
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById('root')
 );
